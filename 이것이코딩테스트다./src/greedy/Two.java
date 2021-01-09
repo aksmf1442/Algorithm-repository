4a5fb8f3f54cfe1a2cb553631d8a48745ca0b857
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-01-09 14:23:44 +0900

    이것이 코딩테스트다 그리디(2번문제)풀이

---
+++ 이것이코딩테스트다./src/greedy/Two.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Two {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    int k = scanner.nextInt();
+    Integer[] data = new Integer[n];
+    for (int i = 0; i < n; i++) {
+      data[i] = scanner.nextInt();
+    }
+    Arrays.sort(data, Collections.reverseOrder());
+    // 가장 큰 수가 더해지는 횟수 계산
+    int cnt = (m / (k + 1)) * k;
+    cnt += m % (k + 1);
+    
+    int answer = 0;
+    answer += cnt * data[0];
+    answer += (m - cnt) * data[1];
+    System.out.println(answer);
+  }
+}
