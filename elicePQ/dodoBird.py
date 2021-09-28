6ab0bcd8e4ee9052b0af1455b42e0e729b2222df
aksmf1442 <aksmf1442@naver.com>
2021-09-29 01:25:50 +0900

    dodoBird solution

---
+++ elicePQ/dodoBird.py
@@ -0,0 +1,15 @@
+n = int(input())
+candidate = list(map(int, input().split()))
+
+count = candidate[0]
+result = []
+result.append(candidate[0])
+for i in range(2, n+1):
+    a = (i * candidate[i-1]) - count
+    count += a
+    result.append(a)
+
+print(" ".join(str(i) for i in result))
+
+
+
