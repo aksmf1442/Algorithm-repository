2fdfb98075b45625047716285a414427472bd603
aksmf1442 <aksmf1442@naver.com>
2021-09-29 01:10:53 +0900

    blueWhiteTeam solution

--- elice/martCounter.py
+++ elice/martCounter.py
@@ -9,26 +9,13 @@ while n >= 1:
     idx = 0
     can = 1000001
     for i in range(len(counter)):
-        if (candidate[i] + counter[i]) < can:
-            idx = i
-            can = candidate[i] + counter[i]
-    candidate[idx] += counter[idx]
+        if (candidate[i] * counter[i]) < can:
+            idx =  i
+            can = candidate * counter[i]
+    candidate[idx] += 1
     n -= 1
 
-candidate.sort()
-print(candidate[len(candidate)-1])
-
-# while n >= 1:
-#     idx = 0
-#     can = 1000001
-#     for i in range(len(counter)):
-#         if (candidate[i] * counter[i]) < can:
-#             idx =  i
-#             can = candidate * counter[i]
-#     candidate[idx] += 1
-#     n -= 1
-
-# for i in range(len(counter)):
-#     if result < counter[i] * candidate[i]:
-#         result = counter[i] * candidate[i]
-# print(result)
+for i in range(len(counter)):
+    if result < counter[i] * candidate[i]:
+        result = counter[i] * candidate[i]
+print(result)
