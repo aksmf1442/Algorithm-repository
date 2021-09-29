54f1bce38f135c1e3a08f0d96952cde9d73ce6dd
aksmf1442 <aksmf1442@naver.com>
2021-09-29 19:24:38 +0900

    stair solution

--- elice/stair.py
+++ elice/stair.py
@@ -1,19 +1,9 @@
-def dfs(li):
-    if sum(li) == n:
-        global result
-        result += 1
-        return
-
-    if sum(li) > n:
-        return
+## 피보나치 수열(메모이제이션과 동일, 일정한 규칙을 찾는게 관건)
     
-    for i in range(1, 3):
-        li.append(i)
-        dfs(li)
-        li.pop()
-    
-
 n = int(input())
-result = 0
-dfs([])
-print(result)
+result = [1, 2]
+
+for i in range(2, n):
+    result.append(result[i-2]+result[i-1])
+
+print(result[n-1])
