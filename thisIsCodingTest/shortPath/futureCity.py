c0169cc500436509e43f1f971baa323c6707fdf6
aksmf1442 <aksmf1442@naver.com>
2021-10-08 21:57:33 +0900

    futureCity solution ver2

--- thisIsCodingTest/shortPath/futureCity.py
+++ thisIsCodingTest/shortPath/futureCity.py
@@ -1,48 +1,77 @@
-import heapq
+# 다익스트라 풀이
 
+# import heapq
+
+# n, m = map(int, input().split())
+# data = [[] for _ in range(n+1)]
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     data[a].append(b)
+#     data[b].append(a)
+
+# x, k = map(int, input().split())
+# INF = int(1e9)
+
+# def daikstra(start, end):
+#     cop = [INF] * (n + 1)    
+    
+#     q = []
+#     heapq.heappush(q, (0, start))
+#     cop[start] = 0
+
+#     while q:
+#         dist, now = heapq.heappop(q)
+
+#         if cop[now] < dist:
+#             continue
+
+#         for i in data[now]:
+#             cost = dist + 1
+            
+
+#             if cost < cop[i]:
+#                 cop[i] = cost
+#                 heapq.heappush(q, (cost, i))
+#     return cop[end]
+
+# result = daikstra(1, k) + daikstra(k, x)
+
+# if result <= INF:
+#     print(result)
+# else:
+#     print(-1)
+
+
+
+# 플루이드 워셜 풀이
+
+INF = int(1e9)
 n, m = map(int, input().split())
-data = [[] for _ in range(n+1)]
+data = [[INF] * (n + 1) for _ in range(n+1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            data[a][b] = 0
+            data[b][a] = 0
 
 for _ in range(m):
     a, b = map(int, input().split())
-    data[a].append(b)
-    data[b].append(a)
+    data[a][b] = 1
+    data[b][a] = 1
 
 x, k = map(int, input().split())
-INF = int(1e9)
 
-def daikstra(start, end):
-    cop = [INF] * (n + 1)    
-    
-    q = []
-    heapq.heappush(q, (0, start))
-    cop[start] = 0
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            data[a][b] = min(data[a][b], data[a][k] + data[k][b])
 
-    while q:
-        dist, now = heapq.heappop(q)
+distance = data[1][k] + data[k][x]
 
-        if cop[now] < dist:
-            continue
-
-        for i in data[now]:
-            cost = dist + 1
-            
-
-            if cost < cop[i]:
-                cop[i] = cost
-                heapq.heappush(q, (cost, i))
-    return cop[end]
-
-result = daikstra(1, k) + daikstra(k, x)
-
-if result <= INF:
-    print(result)
-else:
+if distance >= INF:
     print(-1)
-
-
-
-
-
-
+else:
+    print(distance)
 
