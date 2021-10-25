93ea8ec11ed817105056cf3c429b5f5738085de3
aksmf1442 <aksmf1442@naver.com>
2021-10-25 12:18:32 +0900

    programmers 부족한 금액 계산하기  solution

---
+++ programmers/level1/money_count.js
@@ -0,0 +1,9 @@
+// 부족한 금액 계산하기
+
+function solution(price, money, count) {
+    var answer = 0;
+    for (let i = 1; i <= count; i++) {
+        answer += i * price;
+    }
+    return money >= answer ? 0 : answer - money;
+}
