7a8b28c8b4c5bb647a8430904b1f6bb32894e3c5
aksmf1442 <aksmf1442@naver.com>
2021-10-27 20:31:24 +0900

    programmers 다트게임 solution

--- programmers/level1/dartGame.js
+++ programmers/level1/dartGame.js
@@ -1,6 +1,53 @@
 // 다트게임
 
 function solution(dartResult) {
-    var answer = 0;
-    return answer;
+    const answer = new Array(3).fill(0);
+    let number = "";
+    let idx = 0;
+    for (let i of dartResult) {
+        if (parseInt(i) >= 0 && parseInt(i) <= 10) {
+            number += i;
+        }
+
+        if (i === "S") {
+            number = parseInt(number);
+            number = number ** 1;
+            answer[idx] = number;
+            idx++;
+            number = "";
+        }
+
+        if (i === "D") {
+            number = parseInt(number);
+            number = number ** 2;
+            answer[idx] = number;
+            idx++;
+            number = "";
+        }
+
+        if (i === "T") {
+            number = parseInt(number);
+            number = number ** 3;
+            answer[idx] = number;
+            idx++;
+            number = "";
+        }
+
+        if (i === "*") {
+            answer[idx - 1] *= 2;
+            if (idx >= 2) {
+                answer[idx - 2] *= 2;
+            }
+
+        }
+
+        if (i === "#") {
+            answer[idx - 1] *= (-1);
+        }
+    }
+    console.log(answer);
+
+    return answer.reduce((a, b) => a += b);
 }
+
+console.log(solution("1D2S#10S"));
